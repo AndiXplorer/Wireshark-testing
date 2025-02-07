3890175e108b7a2603559925ca0cc93ea856d51f
AndiXplorer <andi_cakolli@outlook.com>
2025-02-07 14:30:15 +0100

    first version

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name;
+  const message = req.body.message;
+
+  console.log(`Received data: Name: ${name}, Message: ${message}`);
+  res.send('Data received successfully!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on http://192.168.10.163:${port}`);
+});
